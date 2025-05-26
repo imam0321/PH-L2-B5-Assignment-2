6c883611d95721be83290b3f95c649e161cbb12a
imam0321 <imam.hossain0321@gmail.com>
2025-05-26 19:12:58 +0600

    Add SQL queries for various problems and cleanup operations in PostgreSQL_Assignment.sql

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -130,3 +130,56 @@ FROM sightings;
 SELECT * FROM sightings WHERE location LIKE '%Pass';
 
 -- problem - 4
+SELECT r.name, COUNT(s.sighting_id) AS total_sightings
+FROM rangers r
+    LEFT JOIN sightings s ON r.ranger_id = s.ranger_id
+GROUP BY
+    r.ranger_id
+ORDER BY r.name;
+
+-- problem - 5
+SELECT common_name
+FROM species
+WHERE
+    species_id NOT IN (
+        SELECT species_id
+        FROM sightings
+    );
+
+-- problem - 6
+SELECT sp.common_name, s.sighting_time, r.name
+FROM
+    sightings s
+    JOIN species sp ON s.species_id = sp.species_id
+    JOIN rangers r ON s.ranger_id = r.ranger_id
+LIMIT 2;
+
+-- problem - 7
+UPDATE species
+SET
+    conservation_status = 'Historic'
+WHERE
+    discovery_date < '1800-01-01';
+
+-- problem - 8
+SELECT
+    sighting_id,
+    CASE
+        WHEN EXTRACT(
+            HOUR
+            FROM sighting_time
+        ) < 12 THEN 'Morning'
+        WHEN EXTRACT(
+            HOUR
+            FROM sighting_time
+        ) BETWEEN 12 AND 17  THEN 'Afternoon'
+        ELSE 'Evening'
+    END AS time_of_day
+FROM sightings;
+
+-- problem - 9
+DELETE FROM rangers
+WHERE ranger_id NOT IN (
+  SELECT DISTINCT ranger_id FROM sightings
+);
+
